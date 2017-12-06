cf876e3c3fba2caaa74b3dafbf399c6c4f5e2598
Vaibhav Ahuja <vaibhavahuja@outlook.com>
2017-12-06 17:32:14 +0530

    draft

---
+++ js/custom.js
@@ -0,0 +1,125 @@
+
+// Timer for yuvaan
+var timer;
+
+var compareDate = new Date();
+compareDate.setDate(compareDate.getDate() + 50); //add the Number of days left. 
+
+timer = setInterval(function() {
+  timeBetweenDates(compareDate);
+}, 1000);
+
+function timeBetweenDates(toDate) {
+  var dateEntered = toDate;
+  var now = new Date();
+  var difference = dateEntered.getTime() - now.getTime();
+
+  if (difference <= 0) {
+
+    clearInterval(timer);
+
+  } else {
+
+    var seconds = Math.floor(difference / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+
+    hours %= 24;
+    minutes %= 60;
+    seconds %= 60;
+
+    $("#days").text(days);
+    $("#hours").text(hours);
+    $("#minutes").text(minutes);
+    $("#seconds").text(seconds);
+  }
+}
+
+// END OF TIMER FOR YUVAAN
+
+
+
+
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets
+});
+
+
+/* Mobile Navigation
+    -----------------------------------------------*/
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+
+/* HTML document is loaded. DOM is ready.
+-------------------------------------------*/
+$(document).ready(function() {
+
+  /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+ /* Parallax section
+    -----------------------------------------------*/
+  function initParallax() {
+    $('#intro').parallax("100%", 0.1);
+    $('#overview').parallax("100%", 0.3);
+    $('#detail').parallax("100%", 0.2);
+    $('#video').parallax("100%", 0.3);
+    $('#speakers').parallax("100%", 0.1);
+    $('#program').parallax("100%", 0.2);
+    $('#register').parallax("100%", 0.1);
+    $('#faq').parallax("100%", 0.3);
+    $('#venue').parallax("100%", 0.1);
+    $('#sponsors').parallax("100%", 0.3);
+    $('#contact').parallax("100%", 0.2);
+
+  }
+  initParallax();
+
+
+  /* Owl Carousel
+  -----------------------------------------------*/
+  $(document).ready(function() {
+    $("#owl-speakers").owlCarousel({
+      autoPlay: 6000,
+      items : 4,
+      itemsDesktop : [1199,2],
+      itemsDesktopSmall : [979,1],
+      itemsTablet: [768,1],
+      itemsTabletSmall: [985,2],
+      itemsMobile : [479,1],
+    });
+  });
+
+
+  /* Back top
+  -----------------------------------------------*/
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+      })
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
